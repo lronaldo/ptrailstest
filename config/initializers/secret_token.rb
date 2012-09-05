fe4e6e2c0c72b25eaa939401476a700c666ec636
ronaldo <ronaldo@cheesetea.com>
2012-09-06 00:12:41 +0200

    Comenzando nueva aplicación

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pokertrainer::Application.config.secret_token = 'ada813356b5d0264a8387e435b5249587ff37ef45239eaa269658da77fc68b4bab4478a78b63b3e786e8af5b879f81a641507019fa71a34cfe9c64709601212e'

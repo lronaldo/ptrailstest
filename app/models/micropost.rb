7e78f9ccfcae26bc47dbd45e83f42edddf3d72b0
ronaldo <ronaldo@cheesetea.com>
2012-09-06 00:07:26 +0200

    Borrando aplicación de prueba anterior

--- app/models/micropost.rb
+++
@@ -1,5 +0,0 @@
-class Micropost < ActiveRecord::Base
-  attr_accessible :content, :user_id
-  belogs_to :user
-  validates :content, :length => { :maximum => 140 }
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Pokertrainer::Application.config.secret_token = '52652374d8bc559313a19327662f3d60c5d924e6fb92ea3470f725d8bfe1ba8f892edc1ef60cff000fced12f60aeb5ae68495465a1bb3a9aadc5df0292aba60a'

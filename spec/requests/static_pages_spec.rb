2280311c0ce7efed83158a46cccf4f2f39f79a9c
ronaldo <ronaldo@cheesetea.com>
2012-09-09 12:25:12 +0200

    Añadida página de About Us, siguiendo un test previo creado como especificación

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -16,4 +15,12 @@ describe "Static pages" do
         page.should have_content('Help')
     end
   end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+        visit '/static_pages/about'
+        page.should have_content('About Us')
+    end
+  end
+
 end

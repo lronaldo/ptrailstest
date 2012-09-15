4ac215df68202cef4699c8cf42f1a3c459ffccfc
ronaldo <ronaldo@cheesetea.com>
2012-09-15 21:24:32 +0200

    * Creados labels de la mesa

--- script/Table.rb
+++ script/Table.rb
@@ -10,13 +10,52 @@ class Table
     attr_accessor :button
     attr_accessor :bb, :ante, :pot
     attr_accessor :x, :y
+    attr_accessor :labels
 
     def initialize(seats = 6)
-        reset(seats)
+        self.createlabels
+        self.reset(seats)
+    end
+
+    # Generate labels
+    def createlabels
+        # TODO: Generate depending on table
+        @labels = {}
+        
+        # POT LABEL
+        l = Label.new "POT", 6
+        l.x = 26
+        l.y = 9
+        @labels[:pot] = l
+
+        # POT INFO
+        @labels[:potinfo] = []
+        (0...3).each do |i|
+            l = Label.new "info", 10
+            l.x = 24
+            l.y = 10 + i
+            l.fg_color = COLOR_BLACK
+            l.bg_color = COLOR_WHITE
+            @labels[:potinfo].push l
+        end
+
+        # BET LABELS
+        px = [ 32, 35, 32, 17, 14, 17 ]
+        py = [  6, 11, 15, 15, 11,  6 ]
+        @labels[:bets] = []
+        (1..6).each do |i|
+            l = Label.new "Bet Player #{i}", 9
+            l.x = px[i-1]
+            l.y = py[i-1]
+            l.attr     = A_NORMAL
+            l.fg_color = COLOR_MAGENTA
+            l.bg_color = COLOR_GREEN
+            @labels[:bets].push l
+        end
     end
 
     ## Reset table
-    def reset(seats)
+    def reset(seats = 6)
         seats = MAX_SEATS if seats > MAX_SEATS
         @seats  = {}
         @bets   = {}
@@ -60,23 +99,32 @@ class Table
     end
 
     ## Render table
-    def render
+    def render(xoff = 0, yoff = 0)
         ## TODO: Consider diferent kinds of tables
 
         # Render table background
-        y = @y + 3
-        SCR.changeColor A_NORMAL, COLOR_GREEN, COLOR_GREEN
+        x = @x + xoff
+        y = @y + yoff + 3
+        SCR.changeColor A_NORMAL, COLOR_BLACK, COLOR_GREEN
         (0..1).each do |i|
-            (0..8).each do |j|
+            (0..7).each do |j|
                 j = 8-j if i == 0
-                SCR.setxy @x + 10 + j, y
+                SCR.setxy x + 10 + j, y
                 SCR.repchar '*', 38 - 2 * j
                 y += 1
             end
         end
         SCR.setDefaultColor
 
-        # Render Pot
-        
+        # Render labels
+        @labels.each do |key, l|
+            if l.is_a?(Array)
+                l.each do |ll|
+                    ll.render @x, @y
+                end
+            else
+                l.render @x, @y
+            end
+        end
     end
 end
